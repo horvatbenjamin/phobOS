a06f7d93a92457f42ee8cd389c16910730aaf073
Benjamin Horvat <horvatbenjamin@gmail.com>
2010-09-21 16:39:59 +0200

    update...

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ rm ./kernel.bin
 rm ./*.o
 
 nasm -f elf -o loader.o loader.s
-gcc -m32 -o kernel.o -c kernel.c -Wextra -Werror -nostdlib -nostartfiles -nodefaultlibs
-ld -melf_i386  -T linker.ld -o kernel.bin loader.o kernel.o
+gcc -m32 -o kernel.o -c kernel.c -Wextra -Werror -nostdlib -nostartfiles -nodefaultlibs -fno-builtin
+gcc -m32 -o system.o -c system.c -Wextra -Werror -nostdlib -nostartfiles -nodefaultlibs -fno-builtin
+gcc -m32 -o screen.o -c screen.c -Wextra -Werror -nostdlib -nostartfiles -nodefaultlibs -fno-builtin
+
+ld -melf_i386  -T linker.ld -o kernel.bin loader.o kernel.o system.o screen.o
--- kernel.c
+++ kernel.c
@@ -1,25 +1,5 @@
-void clear_screen(unsigned char *vmem){
-	int x;
-	for(x=0;x<80*40;x++){
-		vmem[2*x]=' ';
-	};
-};
-
-void print(unsigned char *vmem,char *text){
-	int row=0,column=0,counter=0;
-
-	while (*(text+counter)!='\0'){
-		if(*(text+counter)=='\n'){
-			column=0;
-			row++;
-			counter++;
-		};
-		vmem[2*column+160*row]=text[counter];
-		vmem[2*column+1+160*row]=0x07;
-		counter++;
-		column++;
-	};
-};
+#include "system.h"
+#include "screen.h"
 
 void kmain( void* mbd, unsigned int magic )
 {
@@ -31,9 +11,8 @@ void kmain( void* mbd, unsigned int magic )
    char * boot_loader_name =(char*) ((long*)mbd)[16];
  
    // Print a letter to screen to see everything is working:
-   unsigned char *videoram = (unsigned char *) 0xb8000;
-   clear_screen(videoram);
    char txt[]="Hello World!!!\nMasodik sor\nHarmadik...\0";
    char *ptr_txt=&txt[0];
-   print(videoram,ptr_txt);
+   init_video();
+   puts(ptr_txt);
 } 
